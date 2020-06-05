a44ea1bc6bb0bf5869b1e2a0716668a8cdf16495
“Duncan <dkrebs@krebsnet.com>
2020-06-05 15:34:23 -0600

    added infinite loop for spring boot demo

--- src/main/java/com/dunktrade/boot/BootApplication.java
+++ src/main/java/com/dunktrade/boot/BootApplication.java
@@ -8,12 +8,21 @@ public class BootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootApplication.class, args);
-		System.out.println("Lets add a breakpoint!!");
-		int i = 0; 
-		if(0 == 0) { 
-			System.out.println("stop here at breakpoint!");
+		// lets test some code here with the 
+		// debugger in eclipse
+		
+		// create a infinite loop
+		int counter = 0; 
+		while(true) { 
+			System.out.println("Loop Count " + counter);
+			counter++;
+			try {
+				Thread.sleep(1000);
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
 		}
-		System.out.println("Hello China!");
+		
 	}
 	
 
