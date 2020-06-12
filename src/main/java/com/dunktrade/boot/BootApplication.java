5d1b1b5ed7982e2e8c156ddea9a8935af3295352
alonlong <alonlong@163.com>
2020-06-12 08:17:53 +0800

    using the slf4j for logging

--- src/main/java/com/dunktrade/boot/BootApplication.java
+++ src/main/java/com/dunktrade/boot/BootApplication.java
@@ -1,26 +1,33 @@
 package com.dunktrade.boot;
 
+import java.lang.invoke.MethodHandles;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.impl.StaticLoggerBinder;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BootApplication {
+public class BootApplication implements CommandLineRunner {
+
+	// SLF4J's logging instance for this class
+	// We could have used LoggerFactory.getLogger(Slf4jSpringBootApplication.class)
+	// as well
+	private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+	// This is what SLF4J uses to bind to a specific logging implementation
+	final StaticLoggerBinder binder = StaticLoggerBinder.getSingleton();
+
+	public void run(String... args) {
+		System.out.println(binder.getLoggerFactory());
+		System.out.println(binder.getLoggerFactoryClassStr());
+
+		LOGGER.debug("hello world");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootApplication.class, args);
-		// lets test some code here with the
-		// debugger in eclipse
-
-		// create a infinite loop
-		int counter = 0;
-		while (true) {
-			System.out.println("Loop Count " + counter);
-			counter++;
-			try {
-				Thread.sleep(1000);
-			} catch (Exception e) {
-				// TODO: handle exception
-			}
-		}
 	}
 }
